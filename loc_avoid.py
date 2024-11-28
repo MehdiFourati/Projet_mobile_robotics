4d2eaecc6f3d45f7774a10bef6631f7d5d744f58
Yilmaz Fatih Selim <fatih.yilmaz@epfl.ch>
2024-11-28 17:38:22 +0100

    local avoidance avec class robot2 marche

--- loc_avoid.py
+++ loc_avoid.py
@@ -1,27 +1,20 @@
 # Local avoidance py file, Fatih
 
-class Robot:
+class Robot2:
     def __init__(self):
         self.center_x = None
         self.center_y = None
         self.alpha = None
         self.robot_width = None
+        #////////////////////
+        self.front_prox = [0,0,0,0,0,0,0]  # Values front prox.horizontal
 
     def update_coordinates(self, center_x, center_y, alpha, robot_width):
         self.center_x = center_x
         self.center_y = center_y
         self.alpha = alpha
         self.robot_width = robot_width
-
-    def get_sensor_value(self, number, prox_value):
-        return self.prox_value[number]
+    #////////////////////
+    def update_front_prox(self, prox_values):
+        self.front_prox = prox_values
     
-    def get_sensors(self, prox):
-        return prox
-    
-
-def update_local_state(sensors, obstThr):
-    for i in range(len(sensors)):
-        if sensors[i] > obstThr:
-            return True  # obstacle detected, state 1 
-    return False
