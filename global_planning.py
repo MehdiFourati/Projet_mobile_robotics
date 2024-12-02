87065b48db6ef4619942158654947dc669990570
Mahdi Fourati <mehdi.fourati1@gmail.com>
2024-12-02 14:34:10 +0100

    new class

--- global_planning.py
+++ global_planning.py
@@ -4,26 +4,21 @@ import numpy as np
 
 
 class Robot:
-    def __init__(self):
+    def _init_(self):
         self.center_x = None
         self.center_y = None
         self.alpha = None
         self.robot_width = None
+        self.front_prox = [0,0,0,0,0,0,0]  # Values front prox.horizontal
 
     def update_coordinates(self, center_x, center_y, alpha, robot_width):
         self.center_x = center_x
         self.center_y = center_y
         self.alpha = alpha
         self.robot_width = robot_width
-
-class goal:
-    def __init__(self):
-        self.center_x = None
-        self.center_y = None
-
-    def update_coordinates(self, center_x, center_y):
-        self.center_x = center_x
-        self.center_y = center_y
+        
+    def update_front_prox(self, prox_values):
+        self.front_prox = prox_values
 
 def obstacle_dictionnary(obstacles):                                             
     """Returns a dictionnary with key : obstacle, value : coordinates"""
