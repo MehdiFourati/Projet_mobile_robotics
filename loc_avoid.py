a8ff4699cd734f3eda7dcff61eeb4daf44d63aed
Mahdi Fourati <mehdi.fourati1@gmail.com>
2024-12-02 15:29:47 +0100

    loc_avoid changes

--- loc_avoid.py
+++ loc_avoid.py
@@ -1,5 +1,3 @@
-
-
 # local_state with the differents states for local avoidance 
 local_state = 1
 DEFAULT = 0
@@ -15,28 +13,6 @@ OBST_DETECTED = 7
 rotationThr = 2000
 obstThrH = 3000
 speed = 150
-
-# Local avoidance py file, Fatih
-
-
-class Robot2:
-    def __init__(self):
-        self.center_x = None
-        self.center_y = None
-        self.alpha = None
-        self.robot_width = None
-        #////////////////////
-        self.front_prox = [0,0,0,0,0,0,0]  # Values front prox.horizontal
-
-    def update_coordinates(self, center_x, center_y, alpha, robot_width):
-        self.center_x = center_x
-        self.center_y = center_y
-        self.alpha = alpha
-        self.robot_width = robot_width
-    #////////////////////
-    def update_front_prox(self, prox_values):
-        self.front_prox = prox_values
-
 
 # Function update proximity values for Thymio
 def update_prox_values(node, aw):
